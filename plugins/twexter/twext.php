3897d17b0075200ff2560027d5276f4f3390a774
Gabriel Saldana <gsaldana@gmail.com>
2008-03-15 17:59:25 -0600

    Changed variable names to something more understandable Changed 'if's comparing with false values, with only boolean 'if' results. (faster) Fixed indentation and removed whitespace

--- plugins/twexter/twext.php
+++ plugins/twexter/twext.php
@@ -2,43 +2,33 @@
 require('twext.neutral.php');
 
 // this is a twext parser. it parses the text/twxt into a tree
-function twext_parse($text1, $text2) {
-	$text1 = explode("\n", $text1);
-	$text2 = explode("\n", $text2);
-	$count1 = count($text1);
-	$count2 = count($text2);
-	$count = max($count1, $count2);
-	
-	$paras = array();
-	$para = false;
+function twext_parse($language_left, $language_right)
+{
+  $language_left = explode("\n", $language_left);
+  $language_right = explode("\n", $language_right);
+  $count1 = count($language_left);
+  $count2 = count($language_right);
+  $count = max($count1, $count2);
+
+  $paras = array();
+  $para = false;
+  $line = false;
+  for($i = 0; $i <= $count; $i++) {
+    $a1 = trim($language_left[$i]);
+    $a2 = trim($language_right[$i]);
+    if(!$a1 && !$a2) { // if its a blank line on both sides
+      if($line) {
+	if (!$para) $para = array();
+	$para[] = $line;
 	$line = false;
-	for ($i = 0; $i <= $count; $i++) {
-/* 		$a1 = $i < $count1 ? trim($text1[$i]) : ''; */
-/* 		$a2 = $i < $count2 ? trim($text2[$i]) : ''; */
-	  $a1 = trim($text1[$i]);
-	  $a2 = trim($text2[$i]);
-	  if (!$a1 && !$a2) { // if its a blank line on both sides
-			if ($line !== false) {
-				if ($para === false) $para = array();
-				$para[] = $line;
-				$line = false;
-			} else if ($para !== false) {
-				$paras[] = $para;
-				$para = false;
-			}
-		} else {
-			if ($line === false) $line = array();
-			$line[] = array($a1, $a2);
-		}
-	}
-/* 	if ($line !== false) { */
-/* 		if ($para === false) $para = array(); */
-/* 		$para[] = $line; */
-/* 		$line = false; */
-/* 	} */
-/* 	if ($para !== false) { */
-/* 		$paras[] = $para; */
-/* 		$para = false; */
-/* 	} */
-	return twext_dom($paras);
+      } else if ($para) {
+	$paras[] = $para;
+	$para = false;
+      }
+    } else {
+      if (!$line) $line = array();
+      $line[] = array($a1, $a2);
+    }
+  }
+  return twext_dom($paras);
 }
