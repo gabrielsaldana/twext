f2cefb47574cc84721eef60fe0f00fb3283d27a1
Gabriel Saldana <gsaldana@gmail.com>
2008-03-20 00:31:57 -0600

    Moved main twexter interface as the index file no need for user authentication to enter twexter

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 define("DB_HOST", "localhost");
 define("DB_NAME", "twexter");
 define("DB_USER", "root");
-define("DB_PASS", "root");
+define("DB_PASS", "b3l1nd4");
 
 define("WWW_PATH", "http://localhost/twexter");
 
