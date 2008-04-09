442e23672e28d6e723bd58d5da5494e57fa0a2bf
Gabriel Saldana <gsaldana@gmail.com>
2008-04-09 01:44:23 -0500

    Imported twext_parse and twext_html to scrolltest Added twexter.css to scrolltest.html

--- xcrolltest.js
+++ xcrolltest.js
@@ -1,5 +1,70 @@
 var scrollbarWidth = 16;
 
+function trim(string) {
+  var str = string.replace(/^\s\s*/, ''),
+    ws = /\s/,
+    i = str.length;
+  while (ws.test(str.charAt(--i)));
+  return str.slice(0, i + 1);
+}
+
+// parses twext into a tree
+function twext_parse(left, right) {
+  var text_left = left.split("\n");
+  var text_right = right.split("\n");
+  var total_lines_text_left = text_left.length;
+  var total_lines_text_right = text_right.length;
+  var max_lines = Math.max(total_lines_text_left, total_lines_text_right);
+
+  var paragraphs = [];
+  var para = false;
+  var line = false;
+  for (var i = 0; i <= max_lines; i++) {
+    var chunk_left = i < total_lines_text_left ? trim(text_left[i]) : '';
+    var chunk_right = i < total_lines_text_right ? trim(text_right[i]) : '';
+    if (!chunk_left && !chunk_right) { // if its a blank line on both sides
+      if (line) {
+	if (!para) para = [];
+	para[para.length] = line;
+	line = false;
+      } else if (para) {
+	paragraphs[paragraphs.length] = para;
+	para = false;
+      }
+    } else {
+      if (!line) line = [];
+      line[line.length] = [chunk_left, chunk_right];
+    }
+  }
+  return paragraphs;
+}
+// takes twext, and generates html for preview
+function twext_html(twext) {
+  var span = document.createElement("span");
+  var html = "<div class='twext-box'>\n";
+  for (var i = 0; i < twext.length; i++) {
+    var para = twext[i];
+    html += "  <div class='twext-para'>\n";
+    for (var j = 0; j < para.length; j++) {
+      var line = para[j];
+      html += "    <div class='twext-line'>\n";
+      for (var k = 0; k < line.length; k++) {
+	var chunk = line[k];
+	html += "      <span class='twext-chunk'>\n";
+	span.textContent = chunk[0];
+	html += "        <span><span class='twext-text'>"+span.innerHTML+"</span></span>\n";
+	span.textContent = chunk[1];
+	html += "        <span><span class='twext-twxt'>"+span.innerHTML+"</span></span>\n";
+	html += "      </span>\n";
+      }
+      html += "    </div>\n";
+    }
+    html += "  </div>\n";
+  }
+  html += "</div>\n";
+  return html;
+}
+
 function getPagePosition (el) {
   var pos = {left: 0, top: 0};
   var par = el.offsetParent;
@@ -37,6 +102,8 @@ function syncTextareasMoz (sb1, sb2, ta1, ta2) {
 			   ta1.scrollTop = this.scrollTop;
 			   ta2.scrollTop = this.scrollTop;
 			   sb2.scrollTop = this.scrollTop;
+			   var preview = document.getElementById('preview');
+			   preview.innerHTML = twext_html(twext_parse(ta1.value,ta2.value));
 			 }
 	sb2.onscroll = function ()
 			 {
