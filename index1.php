f802714a0605d56c277694377529c839cbd97f57
Gabriel Saldana <gsaldana@gmail.com>
2008-04-23 00:11:31 -0500

    Moved all javascript code to js file Removed old code from twexter.js

--- index1.php
+++ index1.php
@@ -2,10 +2,6 @@
   <head>
     <title>Twexter</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <script src="twexter/jquery.js" language="javascript" type="text/javascript"></script>
-    <script src="example.js" language="javascript"
-  type="text/javascript"></script>
-    <script src="twexter.js" language="javascript" type="text/javascript"></script>
     <link rel="stylesheet" href="twext.css" type="text/css">
   </head>
   <body>
@@ -15,26 +11,7 @@
     <textarea name='text2' id='xscroll-right' onkeyup="javascript:growTextAreas('xscroll-right','xscroll-left')"><?php echo htmlspecialchars($post['text2']);?></textarea>
     <input type='button' value='twext me' onclick="javascript:twext_text();"/>
   </body>
-<script language="javascript" type="text/javascript">
-
-var left = document.getElementById('xscroll-left');
-var right = document.getElementById('xscroll-right');
-var preview = document.getElementById('preview');
-left.value = text1;
-right.value = text2;
-
-
-left.style.height = left.scrollHeight + 25 + 'px';
-right.style.height = right.scrollHeight +25 + 'px';
-
-
-function growTextAreas(thistextarea,othertextarea)
-{
-  var thistextarea = document.getElementById(thistextarea);
-  var other = document.getElementById(othertextarea);
-
-  thistextarea.style.height = thistextarea.scrollHeight + 'px';
-  other.style.height = thistextarea.style.height;
-}
-</script>
+    <script src="twexter/jquery.js" language="javascript" type="text/javascript"></script>
+    <script src="example.js" language="javascript" type="text/javascript"></script>
+    <script src="twexter.js" language="javascript" type="text/javascript"></script>
 </html>
--- twexter.js
+++ twexter.js
@@ -1,5 +1,20 @@
-var scroll1 = 0; var scroll2 = 0;
-var loaded = false;
+var left = document.getElementById('xscroll-left');
+var right = document.getElementById('xscroll-right');
+var preview = document.getElementById('preview');
+left.value = text1;
+right.value = text2;
+
+left.style.height = left.scrollHeight + 25 + 'px';
+right.style.height = right.scrollHeight +25 + 'px';
+
+function growTextAreas(thistextarea,othertextarea)
+{
+  var thistextarea = document.getElementById(thistextarea);
+  var other = document.getElementById(othertextarea);
+
+  thistextarea.style.height = thistextarea.scrollHeight + 'px';
+  other.style.height = thistextarea.style.height;
+}
 
 // better and faster trim function
 // taken from http://blog.stevenlevithan.com/archives/faster-trim-javascript
@@ -66,29 +81,6 @@ function twext_html(twext) {
   html += "</div>\n";
   return html;
 }
-//takes twext, and generates xml for preview
-function twext_xml(twext) {
-  var xml = "<twext>\n";
-  for(var i=0;i < twext.length;i++) {
-    var para = twext[i];
-    xml += "  <para>\n";
-    for(var j=0;j < para.length;j++) {
-      var line = para[j];
-      xml += "    <line>\n";
-      for(var k=0;k< line.length;k++) {
-	var chunk = line[k];
-	xml += "      <chunk>\n";
-	xml += "        <text>"+chunk[0]+"</text>\n";
-	xml += "        <twxt>"+chunk[1]+"</twxt>\n";
-	xml += "      </chunk>\n";
-      }
-      xml += "    </line>\n";
-    }
-    xml += "  </para>\n";
-  }
-  xml += "</twext>\n";
-  return xml;
-}
 // scrolls preview according to text/twxt scroll position
 function scrollTo(to, from) {
   var percent = (from.scrollHeight == from.clientHeight) ? 0 : from.scrollTop * 100 / (from.scrollHeight - from.clientHeight);
@@ -99,111 +91,7 @@ function _scrollTo(to, from) {
   var percent = (from.scrollHeight == from.clientHeight) ? 0 : from.scrollTop * 100 / (from.scrollHeight - from.clientHeight);
   return to.scrollTop = percent * (to.scrollHeight - to.clientHeight) / 100;
 }
-// fixes tables problem by calculating and setting negative right margins for chunks
-function solveTablesProblem(xcroll) {
-  $(".twext-line", xcroll).each(function() {
-      var chunks = $(".twext-chunk", this);
-      for (var i = 0; i < chunks.length-1; i++) {
-	var a = chunks.eq(i);
-	var b = chunks.eq(i+1);
-	var a_text = $(".twext-text", a).width() / 2;
-	var a_twxt = $(".twext-twxt", a).width() / 2;
-	var b_text = $(".twext-text", b).width() / 2;
-	var b_twxt = $(".twext-twxt", b).width() / 2;
-	var compare = (a_text - a_twxt) * (b_text - b_twxt);
-	if (compare < 0) {
-	  var cdiff = Math.max(a_text, a_twxt) + Math.max(b_text, b_twxt);
-	  var fdiff = Math.max(a_text + b_text, a_twxt + b_twxt);
-	  var diff = cdiff - fdiff;
-	  chunks.eq(i).css('margin-right', '-' + (diff-1) + 'px');
-	}
-      }
-    });
-}
-// main xcroll setup function
-function fixXcrolls() {
-  // loop over all xcrolls
-  $(".xcroll").each(function() {
-      var div = $(this);
-      var preview = div.children(".xcroll-preview");
-      var totalWidth = div.width();
-      var left = div.children(".xcroll-left"), right = div.children(".xcroll-right");
-      var leftText = left.get(0), rightText = right.get(0);
-      var scrollBarWidth = left.width() - leftText.clientWidth;
-      var textAreaWidth = totalWidth - scrollBarWidth;
-      var leftWidth = Math.floor(textAreaWidth/2);
-      var rightWidth = textAreaWidth - leftWidth;
-      left.css("width", leftWidth + scrollBarWidth);
-      right.css("width", rightWidth + scrollBarWidth);
-      right.css("margin-right", -scrollBarWidth);
-      totalWidth = div.width();
-      // hook text areas
-      if (leftText.onscroll == null) { // this only runs once, it basically does function hookup
-	var leftFunc = function() {
-	  scrollTo(preview.children(".twext-box").get(0), leftText);
-	  return _scrollTo(rightText, leftText);
-	}
-	var rightFunc = function() {
-	  scrollTo(preview.children(".twext-box").get(0), rightText);
-	  return _scrollTo(leftText, rightText);
-	}
-	// install scroll hooks
-	left.scroll(leftFunc);
-	right.scroll(rightFunc);
-	leftText.onscroll = leftText.onmousemove = leftText.onkeypress = leftFunc;
-	rightText.onscroll = rightText.onmousemove = rightText.onkeypress = rightFunc;
-	var scrollPos = 0, leftStr, rightStr;
-	//$("#status").text("installed");
-	// a timer to check for any kind of modification. this is needed because firefox doesn't support the onscroll event for textareas
-	// a workaround could be using an editable element other than a textarea, but that would require consideration of rich text copy/pasting
-	var timer = setInterval(function() {
-	    // check if width changed
-	    if (totalWidth != div.width()) {
-	      fixXcrolls();
-	      totalWidth = div.width();
-	    }
-	    // check if scroll position changed
-	    if (leftText.scrollTop != scrollPos) {
-	      scrollPos = leftFunc();
-	    } else if (rightText.scrollTop != scrollPos) {
-	      scrollPos = rightFunc();
-	    }
-	  }, 1000);
-	// hook to update preview
-	if (!loaded && (left.val() != leftStr || right.val() != rightStr)) {
-	  preview.html(twext_html(twext_parse(rightStr = right.val(), leftStr = left.val())));
-	  solveTablesProblem(div);
-	  scrollTo(preview.children(".twext-box").get(0), leftText);
-	  //$("#status").text("preview update; " + new Date());
-	  loaded = true;
-	}
-	// hook tabbing. searches the line with the cursor, and moves to the same line in the other textarea
-	var tab = function(from, to) {
-	  var line = from.val().substring(0, from.get(0).selectionEnd).split("\n").length-1;
-	  var str = to.val().split("\n");
-	  var pos = 0;
-	  for (var i = 0; i < line; i++) pos += str[i].length + 1;
-	  to.get(0).selectionEnd = to.get(0).selectionStart = pos;
-	  to.focus();
-	  return false;
-	}
-	left.keydown(function(x) {if (x.keyCode == 9) return tab(left, right);});
-	right.keydown(function(x) {if (x.keyCode == 9) return tab(right, left);});
-      }
-    });
-}
 function twext_text()
 {
-//   $(".xcroll").each(function() {
-//     var left = $(this).children(".xcroll-left");
-//        var right = $(this).children(".xcroll-right");
-//        var preview = $(this).children(".xcroll-preview");
-//        preview.html(twext_html(twext_parse(left.val(),right.val())));
-//      });
-preview.innerHTML = twext_html(twext_parse(left.value,right.value));
+   preview.innerHTML = twext_html(twext_parse(left.value,right.value));
 }
-// initial function, called when document is loaded
-$(document).ready(function(){
-		    fixXcrolls();
-		    twext_text;
-		  });
