4417d9fa6677cfc865133a41459945c7fdc6511e
gabrielsaldana <gabrielsaldana@2f62a6f2-632e-0410-acc4-be7c51fbfb5f>
2008-06-12 05:54:09 +0000

    Added a basic loading interface implemented in php
    git-svn-id: https://twexter.svn.sourceforge.net/svnroot/twexter@15 2f62a6f2-632e-0410-acc4-be7c51fbfb5f

---
+++ load.php
@@ -0,0 +1,32 @@
+<?
+if ($handle = opendir('saved')) {
+    $files = array();
+    /* This is the correct way to loop over the directory. */
+    while (false !== ($file = readdir($handle))) {
+        /* avoid system's . and .. directories (UNIX filesytems) */
+        if($file != '.' && $file != '..')
+            {
+                $files[] = $file;
+            }
+    }
+    closedir($handle);
+}
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+  <head>
+    <title>Twexter</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
+<h1>Saved Twexts</h1>
+<form action="index.php" method="post">
+<ul>
+<? foreach($files as $file): ?>
+<li><input type="radio" name="script" value="<?= $file ?>"><label><?= $file ?></label></li>
+<? endforeach ?>
+</ul>
+<input type="submit" value="Load">
+</form>
+</body>
+</html>
